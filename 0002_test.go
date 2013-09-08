aad6220b744bf9f188667699672edff9ee3eeac6
Stefan Warten <sw@00101010.org>
2013-09-08 18:25:26 +0200

    more tests

--- 0002_test.go
+++ 0002_test.go
@@ -3,14 +3,17 @@ package main
 import "testing"
 
 func TestP2(t *testing.T) {
-	in, out := 100, 44
-	if x := p2(in); x != out {
-		t.Errorf("p1(%d) = %d, should be %d", in, x, out)
+	pairs := [...][2]int{{100, 44}, {4000000, 4613732}}
+	for _, pair := range pairs {
+		in, out := pair[0], pair[1]
+		if x := p2(in); x != out {
+			t.Errorf("p2(%d) = %d, should be %d", in, x, out)
+		}
 	}
 }
 
 func BenchmarkP2(b *testing.B) {
-	in := 1000
+	in := 4000000
 	for i := 0; i < b.N; i++ {
 		p2(in)
 	}
