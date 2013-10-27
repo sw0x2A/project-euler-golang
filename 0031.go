d669c9ddd6e0039a786f1753980dc76e50d54991
Stefan Warten <sw@00101010.org>
2013-10-27 17:09:42 +0100

    Use wizeman's algorithm

--- 0031.go
+++ 0031.go
@@ -3,21 +3,13 @@
 package main
 
 func p31(n int) int {
-	sum := 0
-	for a := n; a >= 0; a -= 200 {
-		for b := a; b >= 0; b -= 100 {
-			for c := b; c >= 0; c -= 50 {
-				for d := c; d >= 0; d -= 20 {
-					for e := d; e >= 0; e -= 10 {
-						for f := e; f >= 0; f -= 5 {
-							for g := f; g >= 0; g -= 2 {
-								sum++
-							}
-						}
-					}
-				}
-			}
+	coins := []int{1, 2, 5, 10, 20, 50, 100, 200}
+	ways := make([]int, n+1)
+	ways[0] = 1
+	for i := range coins {
+		for j := coins[i]; j <= n; j++ {
+			ways[j] = ways[j] + ways[j-coins[i]]
 		}
 	}
-	return sum
+	return ways[n]
 }
