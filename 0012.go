d02f318d83fc7296505c8fc15487d9e1cfc715ae
Stefan Warten <sw@00101010.org>
2013-10-27 11:11:02 +0100

    Use Sieve of Eratosthenes instead

--- 0012.go
+++ 0012.go
@@ -2,11 +2,9 @@
 
 package main
 
-import "math"
-
 func p12() int {
 	n, dn, cnt := 3, 2, 0
-	primearray := sieveOfAtkin()
+	primearray := PrimesBelow(1000000)
 	for cnt <= 500 {
 		n++
 		n1 := n
@@ -35,50 +33,4 @@ func p12() int {
 		dn = dn1
 	}
 	return n * (n - 1) / 2
-}
-
-func sieveOfAtkin() []int {
-	limit := 1000000
-	limitSqrt := int(math.Sqrt(float64(limit)))
-	sieve := make([]bool, limit+1)
-	sieve[0], sieve[1], sieve[2], sieve[3] = false, false, true, true
-	for x := 1; x <= limitSqrt; x++ {
-		for y := 1; y <= limitSqrt; y++ {
-			n := 4*x*x + y*y
-			if n <= limit && (n%12 == 1 || n%12 == 5) {
-				sieve[n] = !sieve[n]
-			}
-
-			n = 3*x*x + y*y
-			if n <= limit && n%12 == 7 {
-				sieve[n] = !sieve[n]
-			}
-
-			n = 3*x*x - y*y
-			if x > y && n <= limit && n%12 == 11 {
-				sieve[n] = !sieve[n]
-			}
-		}
-	}
-	for n := 5; n <= limitSqrt; n++ {
-		if sieve[n] {
-			x := n * n
-			for i := x; i <= limit; i += x {
-				sieve[i] = false
-			}
-		}
-	}
-	np := 65500
-	primes := make([]int, np)
-	c := 0
-	for i := 0; i <= limit; i++ {
-		if sieve[i] {
-			primes[c] = i
-			c++
-			if c >= np {
-				break
-			}
-		}
-	}
-	return primes
 }
